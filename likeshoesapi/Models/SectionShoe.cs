8bc739f28b5e292e06347fc7a3a17b53083be33f
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2024-03-13 23:27:25 -0500

    ...

---
+++ likeshoesapi/Models/SectionShoe.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace likeshoesapi.Models
+{
+    public class SectionShoe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string? Section {  get; set; }
+
+        [Required]
+        [StringLength(20)]
+    }
+}
+
+//{
+//nameButton: "Mujeres",
+//      sections:
+//    [
+//        {
+//    nameSection: "Tenis",
+//          sectionElements: ["Cerrados", "Sin cordon"],
+//        },
+//        {
+//    nameSection: "Botas",
+//          sectionElements: [],
+//        },
+//      ],
+//    },
